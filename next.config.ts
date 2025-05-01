f3f847b2a7a0368b150915127fe5dc88ae206cbe
Dennis <dennisdiepolder@outlook.com>
2025-05-01 21:17:24 +0200

    clerk envs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    CLERK_SIGN_IN_URL: `${BASE_URL}/sign-in`,
+    CLERK_SIGN_UP_URL: `${BASE_URL}/sign-up`,
+    CLERK_AFTER_SIGN_IN_URL: `${BASE_URL}/dashboard`,
+    CLERK_AFTER_SIGN_UP_URL: `${BASE_URL}/`,
+  },
 };
 
 export default nextConfig;
